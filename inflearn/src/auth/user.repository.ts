5a161858b226f8dfffd5b583daca4d64ff7fca5a
조성원 <tjddnjsjo@kakao.com>
2023-08-31 17:17:27 +0900

    feat: SHA256 + Salt 암호화

--- inflearn/src/auth/user.repository.ts
+++ inflearn/src/auth/user.repository.ts
@@ -6,6 +6,7 @@ import {
   InternalServerErrorException,
 } from '@nestjs/common';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class UserRepository extends Repository<User> {
@@ -15,7 +16,10 @@ export class UserRepository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
-    const user = this.create({ username, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
 
     try {
       await this.save(user);
